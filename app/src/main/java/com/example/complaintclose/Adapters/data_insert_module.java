62059265af3f4eb967ccb55dfcd55ddb2e271f2b
guddu <guddulawaniya123@gmail.com>
2024-02-26 16:28:36 +0530

    complete

---
+++ app/src/main/java/com/example/complaintclose/Adapters/data_insert_module.java
@@ -0,0 +1,12 @@
+package com.example.complaintclose.Adapters;
+
+public class data_insert_module {
+    String gpdata,itemdata,qutydata,serialno;
+
+    public data_insert_module(String gpdata, String itemdata, String qutydata, String serialno) {
+        this.gpdata = gpdata;
+        this.itemdata = itemdata;
+        this.qutydata = qutydata;
+        this.serialno = serialno;
+    }
+}
